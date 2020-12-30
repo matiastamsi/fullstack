78c5695ae2f180c6602e26034c98be58ac949193
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2020-12-30 02:13:41 +0200

    Part 2 started and done 2.1 kurssitiedot step6.

---
+++ part2/kurssitiedot/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Course from './components/Course'
+
+const App = ({ courses }) => {
+  return (
+
+    < div >
+      <ul>
+        {courses.map(course =>
+          <Course key={course.id} course={course} />
+        )}
+      </ul>
+    </div >
+  )
+}
+
+export default App
